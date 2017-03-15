2dbe7f24f1039363d49fdb5c462cbf86cb8b4e6f
JbIPS <jeanbaptiste.richardet@gmail.com>
2017-03-15 15:44:31 +0100

    Starting point

--- server/app.js
+++ server/app.js
@@ -22,48 +22,9 @@ server.use(restify.bodyParser({
 server.use(restify.CORS());
 
 server.get('/photos', (req, res, next) => {
-  const response = [];
-  Fs.readdir(PHOTOS_DIR, (err, images) => {
-    // Filter-out hidden files
-    images.filter((img) => !img.startsWith('.'))
-    .forEach((i) => {
-      response.push({
-        title: i,
-        url: Path.join(PHOTOS_DIR, i)
-      });
-    });
-    res.send(200, response);
-  });
 });
 
 server.post('/photos', (req, res, next) => {
-  let extension;
-  switch(req.files.file.type) {
-    case 'image/jpeg':
-      extension = '.jpg';
-      break;
-    case 'image/png':
-      extension = '.png';
-      break;
-    case 'image/gif':
-      extension = '.gif';
-      break;
-    default:
-      extension = null;
-  }
-  if(extension !== null) {
-    Fs.createReadStream(req.files.file.path).pipe(Fs.createWriteStream(Path.join(PHOTOS_DIR, req.body.title + extension)))
-    .on(() => {
-      res.send(200);
-    })
-    .on((err) => {
-      console.error('ERROR', err);
-      res.send(500);
-    });
-  } else {
-    console.log('Invalid format ' + req.files.file.type);
-    res.send(400);
-  }
 });
 
 server.listen(3000);
