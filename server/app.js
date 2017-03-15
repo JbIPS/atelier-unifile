eabcdcf71d7bf6df24c8c235239cf220f561dd6e
JbIPS <jeanbaptiste.richardet@gmail.com>
2017-03-15 21:13:19 +0100

    Post Atelier

--- server/app.js
+++ server/app.js
@@ -22,11 +22,56 @@ server.use(restify.bodyParser({
 server.use(restify.CORS());
 
 server.get('/photos', (req, res, next) => {
+  Fs.readdir(PHOTOS_DIR, (err, files) => {
+     const photos = files.reduce((memo, file) => {
+       if(!file.startsWith('.')) {
+         memo.push({
+           url: Path.join(PHOTOS_DIR, file),
+           title: file.split('.')[0]
+         });
+       }
+       return memo;
+     }, []);
+    res.send(photos);
+  });
 });
 
 server.post('/photos', (req, res, next) => {
+  console.log(req.params.title);
+  console.log(req.files.file);
+  let extension;
+  switch(req.files.file.type) {
+    case 'image/gif':
+      extension = '.gif';
+      break;
+    case 'image/jpeg':
+      extension = '.jpg';
+      break;
+    case 'image/png':
+      extension = '.png';
+      break;
+    default:
+      extension = null;
+  }
+  if(extension != null) {
+    Fs.createReadStream(req.files.file.path)
+      .pipe(Fs.createWriteStream(Path.join(PHOTOS_DIR, req.params.title + extension)))
+      .on('error', (err) => {
+        console.log('ERROR', err);
+      })
+      .on('finish', () => {
+        console.log('Fini');
+        res.send(201);
+      });
+  } else {
+    res.send(400, `Not supported type: ${req.files.file.type}`);
+  }
 });
 
+server.get('/photos/:title', (req, res, next) => {
+  Fs.createReadStream(Path.join(PHOTOS_DIR, req.params.title))
+  .pipe(res);
+});
 
 server.get(/\/.*/, restify.serveStatic({
   directory: './public',
